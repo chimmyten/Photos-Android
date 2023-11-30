c9030f91f8d8ae9ad5e17303efcdaf7fc15347fe
SpencerTong <slt140@scarletmail.rutgers.edu>
2023-11-30 15:52:49 -0500

    recycler view for album implemented

--- app/src/main/java/com/example/photos/Album.java
+++ app/src/main/java/com/example/photos/Album.java
@@ -2,12 +2,15 @@ package com.example.photos;
 
 import android.os.Bundle;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import java.sql.Array;
 import java.util.ArrayList;
 
 public class Album extends MainActivity{
     ArrayList<PhotoModel> photoModelsArrayList = new ArrayList<>();
-    int[] photos = {R.drawable.ic_launcher_foreground};
+    int[] photos = {R.drawable.ic_launcher_foreground, R.drawable.ic_launcher_background};
     String albumName;
 
     @Override
@@ -15,11 +18,22 @@ public class Album extends MainActivity{
         super.onCreate(savedInstance);
         setContentView(R.layout.album_page);
 
+        RecyclerView recyclerView = findViewById(R.id.photoRecyclerView);
+
         setUpPhotos();
+
+        album_page_recycler_view_adapter adapter = new album_page_recycler_view_adapter(this, photoModelsArrayList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 
     public void setUpPhotos(){
-        photoModelsArrayList.add(new PhotoModel("potato", photos[0]));
+
+        photoModelsArrayList.add(new PhotoModel("potato1", photos[0]));
+        photoModelsArrayList.add(new PhotoModel("potato2", photos[1]));
+        photoModelsArrayList.add(new PhotoModel("potato3", photos[0]));
+
+
     }
 
 }
--- app/src/main/java/com/example/photos/album_page_recycler_view_adapter.java
+++ app/src/main/java/com/example/photos/album_page_recycler_view_adapter.java
@@ -1,5 +1,7 @@
 package com.example.photos;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,23 +10,35 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class album_page_recycler_view_adapter extends RecyclerView.Adapter<album_page_recycler_view_adapter.MyViewHolder> {
+    Context context;
+    ArrayList<PhotoModel> photoModels;
+    public album_page_recycler_view_adapter(Context context, ArrayList<PhotoModel> photoModels){
+        this.photoModels = photoModels;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public album_page_recycler_view_adapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         //inflate the layout (give look to our rows)
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.album_page_recycler_view_row, parent, false);
+        return new album_page_recycler_view_adapter.MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull album_page_recycler_view_adapter.MyViewHolder holder, int position) {
         //assign values to views we created in the recycler view based on postion of recycler view
+        holder.photo.setImageResource(photoModels.get(position).getImage());
     }
 
     @Override
     public int getItemCount() {
         //recycler view gets how many total items want to be displayed
-        return 0;
+        return photoModels.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
@@ -32,9 +46,10 @@ public class album_page_recycler_view_adapter extends RecyclerView.Adapter<album
         //similar to onCreate
 
         ImageView photo;
-        TextView caption;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            photo = itemView.findViewById(R.id.imageView);
+
         }
     }
 
